038330679fa78ee635a042dd930dc394f0e0a4ad
zacharysarette <zacharywsarette@gmail.com>
2021-02-14 02:24:51 +0900

    Adds more error throwing

--- src/generateObject.js
+++ src/generateObject.js
@@ -1,7 +1,9 @@
 const getValues = require('./getValues')
 
 module.exports = ({ fields, objectString, last = false }) => {
+  if (!fields) throw Error('No fields found')
   const values = getValues({ data: fields })
+  if (!values) throw Error('No field data found')
   if (values.length !== fields.length) throw new Error('Unexpected length for values and fields')
   objectString += '{'
   for (let i = fields.length - 1; i > -1; i--) {
